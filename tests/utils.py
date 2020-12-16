c5679f49aa2838ca1d206fb485ba48d85a126011
Sven Eberth <mail@sveneberth.de>
2020-12-16 17:12:48 +0100

    Replace deprecated assertEquals by assertEqual

--- tests/utils.py
+++ tests/utils.py
@@ -6,6 +6,6 @@ class TestUtils(unittest.TestCase):
 	def test_escapeString(self):
 		from viur.core.utils import escapeString
 
-		self.assertEquals("None", escapeString(None))
-		self.assertEquals("abcde", escapeString("abcdefghi", maxLength=5))
-		self.assertEquals("&lt;html&gt;&&lt;/html&gt;", escapeString("<html>\n&\0</html>"))
+		self.assertEqual("None", escapeString(None))
+		self.assertEqual("abcde", escapeString("abcdefghi", maxLength=5))
+		self.assertEqual("&lt;html&gt;&&lt;/html&gt;", escapeString("<html>\n&\0</html>"))
